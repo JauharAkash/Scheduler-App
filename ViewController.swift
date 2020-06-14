3d0e782dafbac2a6ffa836ec5a9e825ac60189e8
Akash Jauhar <40002973+JauharAkash@users.noreply.github.com>
2020-06-14 08:42:01 -0400

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,164 @@
+//
+// Akash Jauhar
+// Final Project - CSC184
+//
+//  ViewController.swift
+//  Scheduler
+//
+//  Copyright © 2020 Akash Jauhar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+
+    }
+    
+
+    // CSC 15 Link
+    @IBAction func button(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=015&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    
+    // CSC 14 Link
+    @IBAction func Button2(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=014&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+     // Math 71 Link
+    @IBAction func Button3(_ sender: Any) {
+        
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=MATH&coursenum=071&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    
+     // CSC 16 Link
+    @IBAction func Button4(_ sender: UIButton) {
+        
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=016&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    // CSC 24 Link
+    @IBAction func Button5(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=024&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    // math73 Link
+    @IBAction func Button6(_ sender: UIButton) {
+        
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=MATH&coursenum=072&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    // CSC 15s Link
+    @IBAction func Button7(_ sender: UIButton) {
+        
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=015S&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    
+    // CSC 17 Link
+    @IBAction func Button8(_ sender: UIButton) {
+        
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=017&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    // CSC 110 Link
+    @IBAction func Button9(_ sender: UIButton) {
+               UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=110&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func Button10(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=163&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button11(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=111&term=201809&level=")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button12(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=112&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    
+    
+    @IBAction func Button13(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=110A&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+        
+    }
+    
+    
+    @IBAction func Button14(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=175&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func Button15(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=120&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBAction func Button16(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=123&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+
+    @IBAction func Button17(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=170&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button18(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=161&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button19(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=190&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button20(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=183&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button21(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=155&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button22(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=115&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button23(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=185&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button24(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=197A&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button25(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=198F&term=202009&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button26(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=188&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button27(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=197B&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func Button28(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/forms/forms_coursedescriptionform.cfm?course=CSC&coursenum=198S&term=202002&level=UG")! as URL, options: [:], completionHandler: nil)
+    }
+    // link for faculty pg
+    @IBAction func Button29(_ sender: UIButton) {
+        UIApplication.shared.open(URL(string:"https://www.hofstra.edu/academics/colleges/seas/computer-science/computer-science-faculty.html")! as URL, options: [:], completionHandler: nil)
+    }
+    
+    
+}
